4dd336886c18fc8b16f2a7cd273d8d47180309a4
suhtjddmlgus124 <suhtjddmlgus124@gmail.com>
2026-07-20 20:44:02 +0900

    FEAT: 첫 커밋

---
+++ backend/config/asgi.py
@@ -0,0 +1,19 @@
+"""
+ASGI config for config project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from django.core.asgi import get_asgi_application
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
+
+application = ASGIStaticFilesHandler(get_asgi_application())
---
+++ backend/config/settings/dev.py
@@ -0,0 +1,7 @@
+from .base import *
+
+
+AUTH_PASSWORD_VALIDATORS = []
+
+
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:5173', 'http://localhost:5173']
