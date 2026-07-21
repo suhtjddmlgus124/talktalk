e015c2985c62c6b0667e2361444a4732d60abc14
suhtjddmlgus124 <suhtjddmlgus124@gmail.com>
2026-07-21 12:13:21 +0900

    FEAT: 프론트엔드 페이지 생성

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+
+const router = createBrowserRouter([]);
+
+export default function App() {
+    return (
+        <RouterProvider router={router} />
+    );
+}
