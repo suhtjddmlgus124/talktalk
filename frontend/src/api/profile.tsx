3cc90977c1b45b2bb28294be0bed1e8ce77f2487
suhtjddmlgus124 <suhtjddmlgus124@gmail.com>
2026-07-21 17:16:29 +0900

    FEAT: profile 조회 query 수정
    - 403 권한 부족으로 요청이 실패했을 때만 profile이 null이 되도록 수정

--- frontend/src/api/profile.tsx
+++ frontend/src/api/profile.tsx
@@ -1,19 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
-import api from "./api";
+import api, { type AxiosError } from "./api";
+import axios from "axios";
 
 
 type Profile = { user: number, nickname: string };
 
 export function useProfileQuery() {
-    return useQuery<Profile | null>({
+    return useQuery<Profile | null, AxiosError>({
         queryKey: ['account', 'profile'],
         queryFn: async () => {
             try {
                 const response = await api.get<Profile>('/api/account/profile/');
                 return response.data;
             }
-            catch {
-                return null;
+            catch(error) {
+                if(axios.isAxiosError(error) && error.response?.status === 403) return null;
+                else throw error;
             }
         },
     });
